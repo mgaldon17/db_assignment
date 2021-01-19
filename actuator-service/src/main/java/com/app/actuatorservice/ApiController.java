ad2729e515a92400d7df7c89d75f3aed14ec45d1
Manuel Galdon <mgaldon49@gmail.com>
2021-01-19 16:46:10 +0100

    add response in JSON, add JSON reader and country_names

--- actuator-service/src/main/java/com/app/actuatorservice/ApiController.java
+++ actuator-service/src/main/java/com/app/actuatorservice/ApiController.java
@@ -3,19 +3,35 @@ package com.app.actuatorservice;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.app.util.JSONParser;
+import com.fasterxml.jackson.core.JsonParser;
+
 @Controller
 public class ApiController {
 	
-	
+
 	@GetMapping("/api/v1/FRA")
 	@ResponseBody
-	public Airport sayHello(@RequestParam(name="name", required=false) String name) {
+	public Airport printAirport(@RequestParam(name="name", required=false) String name) {
+
 	    return new Airport("FRA", 50.050735, 8.570773, "Frankfurt", 364, "www.Frankfurt-airport.com", "Hessen", "Germany");
 	  }
+	
+	@GetMapping("/api/v1/countries_name")
+	@ResponseBody
+	public Map<String, Object> printCountries(@RequestParam(name="name", required=false) String name) {
+		
+		
+		Map<String, Object> map = JSONParser.read();
+		
+	    return map;
+	  }
+	
 }
---
+++ actuator-service/src/main/java/com/app/util/JSONParser.java
@@ -0,0 +1,51 @@
+package com.app.util;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.tomcat.util.json.ParseException;
+
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
+
+
+public class JSONParser {
+	
+	static String json = "...";
+	
+	public static Map<String, Object> read() {
+		try {
+		    // create a reader
+		    Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/requests/state.json"));
+
+		    // create parser
+		    JsonObject parser = (JsonObject) Jsoner.deserialize(reader);
+		    Map<String, Object> map = new HashMap<String, Object>();
+		    
+		    for (Object key : parser.keySet()) {
+		    	
+	    		String keyStr = (String) key;
+	    		Object keyvalue = parser.get(keyStr);
+		        System.out.println(keyStr + " : " + keyvalue);
+		    	map.put(keyStr, keyvalue);
+		        
+		    }
+		    
+		    reader.close();
+		    return map;
+		} catch (Exception ex) {
+		    ex.printStackTrace();
+		}
+		return null;
+	}    
+
+}
