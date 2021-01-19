1e2893bf37f62e3f7a9de0ce886230cca3449adc
Manuel Galdon <mgaldon49@gmail.com>
2021-01-19 10:17:55 +0100

    Fix pom

---
+++ actuator-service/src/main/java/com/app/actuatorservice/ActuatorServiceApp.java
@@ -0,0 +1,13 @@
+package com.app.actuatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorServiceApp{
+
+  public static void main(String[] args) {
+	    SpringApplication.run(ActuatorServiceApp.class, args);
+  }
+
+}
