632e24effead8c6bc9a1312d6e6c1dc2f6673852
Manuel Galdon <mgaldon49@gmail.com>
2021-01-20 19:34:34 +0100

    add path tests

--- actuator-service/src/test/java/com/app/tests/JUnitControllerTests.java
+++ actuator-service/src/test/java/com/app/tests/JUnitControllerTests.java
@@ -1,16 +1,13 @@
 package com.app.tests;
 
-import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
 import java.util.Map;
 
-import org.hamcrest.collection.IsMapContaining;
-import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
 import com.app.actuatorservice.ApiController;
-import com.github.cliftonlabs.json_simple.JsonObject;
 
 public class JUnitControllerTests {
 	
@@ -18,30 +15,42 @@ public class JUnitControllerTests {
 	public void testAirport() {
 		ApiController apiController= new ApiController();
 		Map<String, Object> map = apiController.printAirport();
-
 		assertTrue(map.size()==4);
-		
-		
 	}
 	
 	@Test
 	public void testCountry() {
 		ApiController apiController= new ApiController();
 		Map<String, Object> map = apiController.printStates();
-
 		assertTrue(map.size()==4);
-		
-		
 	}
 	
 	@Test
 	public void testState() {
 		ApiController apiController= new ApiController();
 		Map<String, Object> map = apiController.printCountries();
-
 		assertTrue(map.size()==3);
-		
-		
 	}
-
+	
+	@Test
+	public void testAirportPath() {
+		
+		File f = new File("src/main/resources/requests/airport.json");
+		assertTrue(f.exists());
+	}
+	
+	@Test
+	public void testCountryPath() {
+		
+		File f = new File("src/main/resources/requests/country.json");
+		assertTrue(f.exists());
+	}
+	
+	@Test
+	public void testStatePath() {
+		
+		File f = new File("src/main/resources/requests/state.json");
+		assertTrue(f.exists());
+	}
+	
 }
